0f3744356124046505724e64da7ca4ddf4dd49cc
LauraMaddaluno <lauralondon.4@gmail.com>
2021-09-09 19:09:05 +0100

    Added test and passed for display secon player hit points

---
+++ spec/features/hit_points_spec.rb
@@ -0,0 +1,10 @@
+feature'view points' do 
+  scenario 'see Player 2 HP' do
+    visit('/')
+    fill_in :player_1_name, with: "Laura"
+    fill_in :player_2_name, with: "Jack"
+    click_button 'Submit'
+   
+    expect(page).to have_content "Jack HP: 60"
+  end
+end 
